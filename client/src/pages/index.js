986084bfdeeb2120e625eb229360875428f7dc9c
Firoz Al Amin <121148879+imfirozalamin@users.noreply.github.com>
2025-05-23 18:38:15 +0530

    fixed_dashboard fix

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -7,8 +7,6 @@ import Trash from "./Trash";
 import Users from "./Users";
 import StatusPage from "./Status";
 import Project from "./Project";
-import Verify from "./Verify";
-import OTPLogin from "./OTPLogin";
 
 export {
   Dashboard,
@@ -20,6 +18,4 @@ export {
   Users,
   StatusPage,
   Project,
-  Verify,
-  OTPLogin,
 };
